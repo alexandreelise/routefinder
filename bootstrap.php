705a57fcf476a79c9c808b7eff02d4b5ecdcccb9
Mr Alexandre J-S William ELISÉ <code@apiadept.com>
2023-09-15 18:24:50 -0400

    Cleanup code, more documentation.

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+/**
+ *
+ * @author        Alexandre ELISÉ <contact@alexandree.io>
+ * @copyright (c) 2009 - present. Alexandre ELISÉ. All rights reserved.
+ * @license       GPL-2.0-and-later GNU General Public License v2.0 or later
+ * @link          https://alexandree.io
+ */
+ini_set('error_reporting', -1);
+defined('APP_DIRECTORY') || define('APP_DIRECTORY', __DIR__);
+defined('API_CONFIG_INI') || define('API_CONFIG_INI', __DIR__ . '/api-config.ini');
